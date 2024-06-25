604565c43b29bb0337b05fc2d8566c9b009ebff4
MustardManZ <mustardmanstuf@gmail.com>
2024-06-25 16:33:17 -0400

    done!

--- We make cards/Assets/PlayerScript.cs
+++ We make cards/Assets/PlayerScript.cs
@@ -1,26 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using Unity.Netcode;
 using UnityEngine;
 
 public class PlayerScript : NetworkBehaviour
 {
+    private NetworkVariable<int> number = new NetworkVariable<int>(1, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
     // Start is called before the first frame update
-    void Start()
+    public override void OnNetworkSpawn()
     {
-        
+        number.OnValueChanged += (int previousValue, int newValue) =>
+        {
+            Debug.Log(OwnerClientId + "; randomNumber; " + number.Value);
+        };
     }
     // Update is called once per frame
     void Update()
     {
         if (!IsOwner) return;
+        if (Input.GetKey(KeyCode.Space)) 
+        {
+            number.Value = Random.Range(0, 100);
+        }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.position += Vector3.right * 0.5f * Time.deltaTime;
+            transform.position += Vector3.right * 5f * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.position += Vector3.left * 0.5f * Time.deltaTime;
+            transform.position += Vector3.left * 5f * Time.deltaTime;
         }
     }
 }
