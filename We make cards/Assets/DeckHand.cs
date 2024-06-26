976e5db50731fe24526a7c81da2f12638cb5c521
lazerclaw321 <mr.ericfang@gmail.com>
2024-06-26 15:28:26 -0400

    ipjohwipojwipojwefoipjwefpoji

--- We make cards/Assets/DeckHand.cs
+++ We make cards/Assets/DeckHand.cs
@@ -14,14 +14,18 @@ public class DeckHand : MonoBehaviour
     void Start()
     {
         //build deck
-        for (int i = 0; i < 20; i++)
+        for (int i = 0; i < 13; i++)
         {
             deck.Add("Person");
         }
 
-        for (int i = 0; i < 20; i++)
+        for (int i = 0; i < 14; i++)
         {
             deck.Add("Knife Guy");
+        }
+        for (int i = 0; i < 13; i++)
+        {
+            deck.Add("Almost Invisible Person");
         }
 
         //shuffle
@@ -40,7 +44,17 @@ public class DeckHand : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        for (int i = 0; i < hand.Count; i++)
+        {
+            if (i <= 4)
+            {
+                hand[i].transform.position = new Vector3(1.6f * i - 9, -3.8f, 0);
+            }
+            else
+            {
+                hand[i].transform.position = new Vector3(1.6f * (i - 5) - 9, -1.9f, 0);
+            }
+        }
     }
 
     [ContextMenu("Draw")]
@@ -48,16 +62,27 @@ public class DeckHand : MonoBehaviour
     {
         for (int i = 0; i < amount; i++)
         {
-            hand.Add(Instantiate(card, transform.position, transform.rotation));
-            if (deck[1] == "Person")
+            if (hand.Count != 10)
             {
-                hand[i].GetComponent<CardScript>().newCard(Sprites[9], "hand", "Person", 2, 2, 2, 1, 4, 1, 0, false, false, false);
+                hand.Add(Instantiate(card, transform.position, transform.rotation));
+                hand[i].name = deck[1];
+
+                //first bool is charge, second bool is stealth, third bool is heroic
+
+                if (deck[1] == "Almost Invisible Person")
+                {
+                    hand[i].GetComponent<CardScript>().newCard(Sprites[1], "hand", "Almost Invisible Person", 1, 3, 2, 2, 4, 1, 0, false, true, false);
+                }
+                if (deck[1] == "Person")
+                {
+                    hand[i].GetComponent<CardScript>().newCard(Sprites[9], "hand", "Person", 2, 2, 2, 1, 4, 1, 0, false, false, false);
+                }
+                else if (deck[1] == "Knife Guy")
+                {
+                    hand[i].GetComponent<CardScript>().newCard(Sprites[5], "hand", "Knife Guy", 3, 2, 3, 3, 4, 1, 0, false, false, false);
+                }
+                deck.RemoveAt(1);
             }
-            else if (deck[1] == "Knife Guy")
-            {
-                hand[i].GetComponent<CardScript>().newCard(Sprites[5], "hand", "Knife Guy", 3, 2, 3, 3, 4, 1, 0, false, false, false);
-            }
-            deck.RemoveAt(1);
         }
     }
 }
